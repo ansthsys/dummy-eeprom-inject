f1f6673aee731fc4121b46652f44d6d6a8a73b6e
ansthsys <azmiythufail91@gmail.com>
2023-09-14 21:18:45 +0700

    add proxy to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api/get": {
+        target: "https://8226-103-129-95-34.ngrok-free.app",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/api/eeprom": {
+        target: "https://6fe4-103-129-95-34.ngrok-free.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
